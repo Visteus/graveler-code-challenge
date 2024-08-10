e414af09a8fe1d3499bf339b2cdf077175b7cf24
Jacob Chaulk <chaulk.jacob@gmail.com>
2024-08-10 18:34:03 -0500

    Update graveler.py

--- graveler.py
+++ graveler.py
@@ -9,7 +9,7 @@ def simulate_turns(ignored):
     return [random.randrange(0,3) for _ in range(0,231)].count(0)
 
 if __name__ == '__main__':
-    with Pool(processes=60) as pool:    # Max processes somewhat dependent on your machine/hardware.
+    with Pool(processes=6) as pool:    # Max processes somewhat dependent on your machine/hardware.
         try:
             start_time = time.time()
             results2 = pool.map(simulate_turns, range(runs))
