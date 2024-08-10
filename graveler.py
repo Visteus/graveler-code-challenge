d9af841462e80adf8324802e57d5930d28401e0e
Jacob Chaulk <chaulk.jacob@gmail.com>
2024-08-10 17:00:04 -0500

    Updated graveler.py

--- graveler.py
+++ graveler.py
@@ -3,25 +3,22 @@ import time
 from itertools import repeat
 from multiprocessing import Process, Pool, TimeoutError
 
-runs = 1000000
+runs = 1000000000
 
-def simulate(ignored):
+def simulate1(ignored):
     roll_counts = [0,0,0,0]
     for i in repeat(None, 231):
         roll = random.randrange(0,3) # equivalent to 4-sided die
         roll_counts[roll] += 1
     return roll_counts[0]
 
-
-
 if __name__ == '__main__':
     start_time = time.time()
-    with Pool(processes=60) as pool:    # Max processes somewhat dependent on your machine/hardware. 
-        results = []
+    with Pool(processes=10) as pool:    # Max processes somewhat dependent on your machine/hardware.
         try:
-            results = pool.map(simulate, range(1,runs))
+            results1 = pool.map(simulate1, range(1,runs))
             run_time = time.time() - start_time
-            print(f'Maximum in {runs}: {max(results)}')
-            print(f"We did it! In only {run_time} seconds")
+            print(f"Maximum in '{runs}' runs with 'simulate1': {max(results1)}")
+            print(f"\tIn only {run_time} seconds")
         except TimeoutError:
             print("TimeoutError")
